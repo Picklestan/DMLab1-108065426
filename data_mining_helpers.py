4e050565bcfb71af2758e4897da9c572a5c9b61c
Picklestan <46674595+Picklestan@users.noreply.github.com>
2019-10-27 04:46:07 +0800

    Add files via upload

---
+++ data_mining_helpers.py
@@ -0,0 +1,48 @@
+import nltk
+from nltk.corpus import stopwords
+"""
+Helper functions for data mining lab session 2018 Fall Semester
+Author: Elvis Saravia
+Email: ellfae@gmail.com
+"""
+
+def format_rows(docs):
+    """ format the text field and strip special characters """
+    D = []
+    for d in docs.data:
+        temp_d = " ".join(d.split("\n")).strip('\n\t')
+        D.append([temp_d])
+    return D
+
+def format_labels(target, docs):
+    """ format the labels """
+    return docs.target_names[target]
+
+def check_missing_values(row):
+    """ functions that check and verifies if there are missing values in dataframe """
+    counter = 0
+    for element in row:
+        if element == True:
+            counter+=1
+    return ("The amoung of missing records is: ", counter)
+
+def tokenize_text(text, remove_stopwords=True):
+    """
+    Tokenize text using the nltk library
+    """
+    tokens=[]
+    stopWords = set(stopwords.words('english'))
+    for d in nltk.sent_tokenize(text, language='english'):
+        for word in nltk.word_tokenize(d, language='english'):
+            word1 = word.lower()
+            if word1 not in stopWords and word1.isdigit()==False:
+                tokens.append(word1)
+    return tokens
+
+def get_top_n_words(corpus, n=None):
+    vec = CountVectorizer(stop_words = 'english').fit(corpus)
+    bag_of_words = vec.transform(corpus)
+    sum_words = bag_of_words.sum(axis=0) 
+    words_freq = [(word, sum_words[0, idx]) for word, idx in vec.vocabulary_.items()]
+    words_freq =sorted(words_freq, key = lambda x: x[1], reverse=True)
+    return words_freq[:n]
